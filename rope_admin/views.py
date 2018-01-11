f421662f80263e3a57993ab8bfaceb391cd623b4
sree522 <sreekanth.t1994@gmail.com>
2018-01-11 23:36:04 +0530

    Adding dependencies

--- rope_admin/views.py
+++ rope_admin/views.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def user_login(request):
+    print request
+    return HttpResponse("Success")
--- rope_ui/static/js/rope.js
+++ rope_ui/static/js/rope.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
+  $("body .loginmodal-submit").on("click", function(event){
+     event.preventDefault();
+     alert("dsfhbhjdsmf");
+  });
 
-}
+
+});
