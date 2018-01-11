67f7be00edeab100b69a8271f2365fdf7e47ac5c
sree522 <sreekanth.t1994@gmail.com>
2018-01-12 00:04:07 +0530

    Adding dynamic ajax call

--- rope_ui/static/js/rope.js
+++ rope_ui/static/js/rope.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
+  var host = "http://0.0.0.0:8856"
+
+  function api_call(url, method, data) {
+
+    var url = host + url
+    $.ajax({
+      url: url,
+      success: function(response) {
+        alert(response);
+      }
+    });
+  }
+
+
   $("body .loginmodal-submit").on("click", function(event){
+     api_call("/user_login/", "POST", {})
      event.preventDefault();
-     alert("dsfhbhjdsmf");
   });
 
 
